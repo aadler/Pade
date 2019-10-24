79fd7940816bb5fb4067f29b23df36ad1533fb05
Avraham Adler <Avraham.Adler@gmail.com>
2019-10-23 22:59:55 -0400

    Please cite if used

---
+++ R/zzz.R
@@ -0,0 +1,3 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("The R core development team and the very active community of package authors have\n invested a lot of time and effort in creating R as it is today. Please give credit\n where credit is due and cite R and R packages when you use them for data analysis.\n The citation for Pade may be created by calling \"citation('Pade')\". Thank you.")
+}
