2ee98f7307cdc4631a81680789cb1c31c252a84e
Avraham Adler <avraham.adler@gmail.com>
2023-01-31 16:04:13 -0500

    Made code more consistent with style guide where possible. Protect with #nolint where not.

--- R/Pade.R
+++ R/Pade.R
@@ -1,19 +1,19 @@
-Pade <- function(L, M, A) {
+Pade <- function(L, M, A) { #nolint
   if (floor(L) != L || floor(M) != M) {
     stop("Polynomial orders need to be integers.")
   }
-  Lp1 <- L + 1L
-  MatSize <- Lp1 + M
-  if (length(A) < MatSize) {
+  lPlus1 <- L + 1L
+  matSize <- lPlus1 + M
+  if (length(A) < matSize) {
     stop("Not enough Taylor series coefficients provided.")
   }
-  PQ <- matrix(0, ncol = MatSize, nrow = MatSize)
-  PQ[1:Lp1, 1:Lp1] <- -diag(Lp1)
+  PQ <- matrix(0, ncol = matSize, nrow = matSize)
+  PQ[1:lPlus1, 1:lPlus1] <- -diag(lPlus1)
   for (i in seq_len(M)) {
-    PQ[, Lp1 + i] <- c(rep.int(0, i), head(A, (MatSize - i)))
+    PQ[, lPlus1 + i] <- c(rep.int(0, i), head(A, (matSize - i)))
   }
-  PadeCoeff <- solve(PQ, -head(A, MatSize))
-  Numer <- head(PadeCoeff, Lp1)
-  Denom <- c(1, tail(PadeCoeff, M))
-  return(list(Px = Numer, Qx = Denom))
+  padeCoeff <- solve(PQ, -head(A, matSize))
+  numer <- head(padeCoeff, lPlus1)
+  denom <- c(1, tail(padeCoeff, M))
+  return(list(Px = numer, Qx = denom))
 }
--- inst/tinytest/test_Pade.R
+++ inst/tinytest/test_Pade.R
@@ -1,43 +1,45 @@
+tol <- sqrt(.Machine$double.eps)
 # Inputs and Expected Outputs
-EXP_0_Taylor <- 1 / factorial(seq_len(11) - 1)
-EXP_0_Pade32Numer <- c(1, 3 / 5, 3 / 20, 1 / 60)
-EXP_0_Pade32Denom <- c(1, -2 / 5, 1 / 20)
-EXP_0_Pade32_Estimate <- Pade(3, 2, EXP_0_Taylor)
-EXP_0_Pade43Numer <- c(1, 4 / 7, 1 / 7, 2 / 105, 1 / 840)
-EXP_0_Pade43Denom <- c(1, -3 / 7, 1 / 14, -1 / 210)
-EXP_0_Pade43_Estimate <- Pade(4, 3, EXP_0_Taylor)
+exp_0_taylor <- 1 / factorial(seq_len(11) - 1)
+exp_0_pade32numer <- c(1, 3 / 5, 3 / 20, 1 / 60)
+exp_0_pade32denom <- c(1, -2 / 5, 1 / 20)
+exp_0_pade32_est <- Pade(3, 2, exp_0_taylor)
+exp_0_pade43numer <- c(1, 4 / 7, 1 / 7, 2 / 105, 1 / 840)
+exp_0_pade43denom <- c(1, -3 / 7, 1 / 14, -1 / 210)
+exp_0_pade43_est <- Pade(4, 3, exp_0_taylor)
 
-LOG1P_0_Taylor = c(0, 1, -1 / 2, 1 / 3, -1 / 4, 1 / 5, -1 / 6)
-LOG1P_0_Pade33Numer = c(0, 1, 1, 11 / 60)
-LOG1P_0_Pade33Denom = c(1, 1.5, 0.6, 0.05)
-LOG1P_0_Pade33_Estimate <- Pade(3, 3, LOG1P_0_Taylor)
+log1p_0_taylor <- c(0, 1, -1 / 2, 1 / 3, -1 / 4, 1 / 5, -1 / 6)
+log1p_0_pade33numer <- c(0, 1, 1, 11 / 60)
+log1p_0_pade33denom <- c(1, 1.5, 0.6, 0.05)
+log1p_0_pade33_est <- Pade(3, 3, log1p_0_taylor)
 
-SIN_Taylor <- c(0, 1/factorial(1), 0, -1/factorial(3), 0, 1/factorial(5), 0,
-                -1/factorial(7), 0, 1/factorial(9), 0, -1/factorial(11))
-SIN_Pade56Numer <- c(0, 1, 0, -2363/18183, 0, 12671/4363920)
-SIN_Pade56Denom <- c(1, 0, 445/12122, 0, 601/872784, 0, 121/16662240)
-SIN_Pade56_Estimate <- Pade(5, 6, SIN_Taylor)
+sin_taylor <- c(0, 1 / factorial(1), 0, -1 / factorial(3), 0, 1 / factorial(5),
+                0, -1 / factorial(7), 0, 1 / factorial(9), 0,
+                -1 / factorial(11))
+sin_pade56numer <- c(0, 1, 0, -2363 / 18183, 0, 12671 / 4363920)
+sin_pade56denom <- c(1, 0, 445 / 12122, 0, 601 / 872784, 0, 121 / 16662240)
+sin_pade56_est <- Pade(5, 6, sin_taylor)
 
 # Testing Accuracy
-expect_equal(EXP_0_Pade32_Estimate[[1]], EXP_0_Pade32Numer)
-expect_equal(EXP_0_Pade32_Estimate[[2]], EXP_0_Pade32Denom)
-expect_equal(EXP_0_Pade43_Estimate[[1]], EXP_0_Pade43Numer)
-expect_equal(EXP_0_Pade43_Estimate[[2]], EXP_0_Pade43Denom)
-expect_equal(LOG1P_0_Pade33_Estimate[[1]], LOG1P_0_Pade33Numer)
-expect_equal(LOG1P_0_Pade33_Estimate[[2]], LOG1P_0_Pade33Denom)
-expect_equal(SIN_Pade56_Estimate[[1]], SIN_Pade56Numer)
-expect_equal(SIN_Pade56_Estimate[[2]], SIN_Pade56Denom)
+expect_equal(exp_0_pade32_est[[1]], exp_0_pade32numer, tolerance = tol)
+expect_equal(exp_0_pade32_est[[2]], exp_0_pade32denom, tolerance = tol)
+expect_equal(exp_0_pade43_est[[1]], exp_0_pade43numer, tolerance = tol)
+expect_equal(exp_0_pade43_est[[2]], exp_0_pade43denom, tolerance = tol)
+expect_equal(log1p_0_pade33_est[[1]], log1p_0_pade33numer, tolerance = tol)
+expect_equal(log1p_0_pade33_est[[2]], log1p_0_pade33denom, tolerance = tol)
+expect_equal(sin_pade56_est[[1]], sin_pade56numer, tolerance = tol)
+expect_equal(sin_pade56_est[[2]], sin_pade56denom, tolerance = tol)
 
 # Testing Errors
-expect_error(Pade(4, 4, LOG1P_0_Taylor),
+expect_error(Pade(4, 4, log1p_0_taylor),
              "Not enough Taylor series coefficients provided.")
-expect_error(Pade(7, 6, SIN_Taylor),
+expect_error(Pade(7, 6, sin_taylor),
              "Not enough Taylor series coefficients provided.")
-expect_error(Pade(4.2, 4, LOG1P_0_Taylor),
+expect_error(Pade(4.2, 4, log1p_0_taylor),
              "Polynomial orders need to be integers.")
-expect_error(Pade(4, 3.4, LOG1P_0_Taylor),
+expect_error(Pade(4, 3.4, log1p_0_taylor),
              "Polynomial orders need to be integers.")
 
 ## Test CITATION
-expect_true(any(grepl(packageVersion('Pade'), toBibtex(citation('Pade')),
+expect_true(any(grepl(packageVersion("Pade"), toBibtex(citation("Pade")),
                       fixed = TRUE)))
