40e7cbee269e8ef0fba949def9606a5eade0f23e
Avraham Adler <Avraham.Adler@gmail.com>
2026-01-22 23:33:02 -0500

    Change default to qr.solve for robustness. Leave solve for when speed is important.

--- R/Pade.R
+++ R/Pade.R
@@ -1,7 +1,7 @@
 # Copyright (c) 2015, Avraham Adler All rights reserved
 # SPDX-License-Identifier: BSD-2-Clause OR GPL-2.0-or-later
 
-Pade <- function(L, M, A) { # nolint object_name_linter
+Pade <- function(L, M, A, method = c("qr", "solve"), tol = NULL) { # nolint object_name_linter
   if (L < 0 || M < 0 || floor(L) != L || floor(M) != M) {
     stop("Polynomial orders need to be non-negative integers.")
   }
@@ -18,6 +18,15 @@ Pade <- function(L, M, A) { # nolint object_name_linter
   headA <- A[seq_len(matSize)]
   for (i in seq_len(M)) {
     PQ[(i + 1):matSize, lPlus1 + i] <- headA[1:(matSize - i)]
+  }
+
+  if (is.null(tol)) tol <- .Machine$double.eps
+  method <- match.arg(method)
+
+  if (method == "solve") {
+    padeCoeff <- solve(PQ, -headA)
+  } else {
+    padeCoeff <- qr.solve(PQ, -headA, tol = tol)
   }
 
   padeCoeff <- solve(PQ, -headA)
